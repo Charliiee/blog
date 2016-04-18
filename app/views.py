5e74636b43af01de96300cb5e6ad52c1e583d9b9
Charliie <charliie@admin.com>
2016-04-17 22:06:54 -0300

    basic templates added

---
+++ app/views.py
@@ -0,0 +1,26 @@
+from . import app
+from flask import render_template
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': "World"}  # fake user
+    posts = [
+        {
+            'author': {'nickname': "Flavio"},
+            'body': "Beatiful day in Portland!"
+        },
+        {
+            'author': {'nickname': "Tinaa"},
+            'body': "The Avengers movie was so cool!"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
