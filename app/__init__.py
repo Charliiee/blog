c1a8120cd6a7f66fd7dbc24a712d900b36929c7a
Charliie <charliie@admin.com>
2016-04-24 21:03:41 -0300

    add file and email logging feature

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from config import  (basedir, ADMINS, MAIL_PASSWORD,
+                     MAIL_PORT, MAIL_SERVER, MAIL_USERNAME)
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
@@ -9,5 +11,36 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler, SMTPHandler
+    # mail logging
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                               'no-reply@' + MAIL_SERVER,
+                               ADMINS,
+                               'blog failure',
+                               credentials
+                               )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+    # file logging
+    file_handler = RotatingFileHandler('tmp/microblog.log',
+                                       'a',
+                                       1 * 1024 * 1024,  # 1 megabyte
+                                       10
+                                       )
+    file_handler.setFormatter(
+        logging.Formatter(('%(asctime)s %(levelname)s %(message)s '
+                          '[in %(pathname)s:%(lineno)d]'))
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('blog startup')
 
 from . import views, models
--- config.py
+++ config.py
@@ -4,13 +4,24 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = 'charliie'
+MAIL_PASSWORD = 'Ch@rl11e'
+
+#administrator list
+ADMINS = ['charliee@admin.com']
+# database settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+# form validation settings
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "you-will-never-guess"
 
+# Oauth social network login settings
 OAUTH_CREDENTIALS = {
     'facebook': {
         'id': '957784927671568',
@@ -22,6 +33,7 @@ OAUTH_CREDENTIALS = {
     }
 }
 
+# openid login settings
 OPENID_PROVIDERS = [
     {'name': "Google", 'url': "https://www.google.com/accounts/o8/id"},
     {'name': "Yahoo", 'url': "https://me.yahoo.com"},
